4106d5d6597420cbd87acb1546729f41c5da75e9
HiQualityStar <aurora980331@gmail.com>
2025-04-07 04:28:17 -0700

    fix: get token request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -55,52 +55,44 @@ const Page = () => {
     if (isRunning) return;
     setIsRunning(true);
 
-    const interval = setInterval(async () => {
-      try {
+    try {
+      while (true) {
         const res = await axios.put("/api/getToken");
         console.log(res.data.isNew, res.data.siteKey);
 
         if (res.data.isNew && res.data.siteKey) {
           const siteKey = res.data.siteKey;
-          console.log("Using siteKey:", siteKey); // Debugging siteKey
-
-          // Wait for the script to load and be ready
           await loadRecaptchaScript(siteKey);
-          await new Promise<void>((resolve) => setTimeout(resolve, 1000));
+          await new Promise((resolve) => setTimeout(resolve, 1000));
           const token = await runRecaptcha(siteKey);
+          console.log(token);
           if (token) {
-            console.log("Token received:", token); // Debugging the token value
-
             const config = {
               headers: {
                 "Content-Type": "application/json",
-                Token: `${token}`,
+                Token: token,
               },
             };
-
-            // Post the token to the API
             await axios.post("/api/getToken", { isToken: true }, config);
           } else {
             console.log("Token was undefined, retrying...");
           }
         }
-      } catch (error: any) {
-        setStatus(`❌ Failed: ${error.message}`);
-        clearInterval(interval);
-        setIsRunning(false);
-      }
-    }, 1000);
 
-    // Cleanup interval on component unmount or when isRunning is false
-    return () => clearInterval(interval);
+        // Wait 1s before next round
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      }
+    } catch (error: any) {
+      setStatus(`❌ Failed: ${error.message}`);
+    } finally {
+      setIsRunning(false);
+    }
   };
 
   return (
     <>
       <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center text-white bg-black/50 backdrop-blur">
-        <h1 className="text-2xl mb-6">
-          ReCaptcha v3 Token Generator
-        </h1>
+        <h1 className="text-2xl mb-6">ReCaptcha v3 Token Generator</h1>
         <button
           onClick={handleGetToken}
           disabled={isRunning}
